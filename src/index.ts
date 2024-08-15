481f271d05507e37d7cfb9c9ba93b3747be5c4b7
cosby1992 <andersbirkedahl@hotmail.com>
2024-08-15 20:33:41 +0200

    feat: added ts wrapper for sub

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-const sumModule = require('../build/Release/sum.node');
+const basicMathModule = require('../build/Release/basic-math.node');
 
 export function sum(a: number, b: number): number {
-  return sumModule.sum(a, b);
+  return basicMathModule.sum(a, b);
+}
+
+export function sub(a: number, b: number): number {
+  return basicMathModule.sub(a, b);
 }
